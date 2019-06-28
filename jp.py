17045921df84bf6a73ae7610a85e4ab53c56a7f4
jpsoundar <51952212+jpsoundar@users.noreply.github.com>
2019-06-28 15:23:25 +0530

    Update jp.py

--- jp.py
+++ jp.py
@@ -1,5 +1,5 @@
 ch= input("Enter a character:")
-if(ch=='A'orch=='a' or ch='Eorch'e'orch=='I'orch=='i'orch=='O'orch=='o'
+if(ch=='A'orch=='a'orch='E'orch'e'orch=='I'orch=='i'orch=='O'orch=='o'
 orch=='U'orch=='u'):
   print(ch, "is a vowel")
 else:
