8d4c4a70607278e2f41e53c576a901836fb2741b
jpsoundar <51952212+jpsoundar@users.noreply.github.com>
2019-06-28 14:58:30 +0530

    Update jp.py

--- jp.py
+++ jp.py
@@ -3,7 +3,7 @@ if(ch=='A' or ch=='a' or ch='E' or  ch'e'  or ch=='I' or ch=='i' or ch=='O' or c
 or ch=='U' or ch=='u'):
   print(ch, "is a vowel")
 else:
-  print(ch, "is a consonant")
+  print(ch, "is Vowel")
   
   
   
