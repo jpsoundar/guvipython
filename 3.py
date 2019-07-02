e3e77bade6a22fcd3e8b13ca06168e2d6df96fb2
jpsoundar <51952212+jpsoundar@users.noreply.github.com>
2019-07-02 11:38:13 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 n=input("enter a character:")
-if((n>='a' and n<='z') or (n>='A' and n<='Z')):
+if((n>='a' and n<='d') or (n>='A' and n<='D')):
   print(ch, "is an Alphabet")
 else:
   print(ch, "is not an Alphabet")
